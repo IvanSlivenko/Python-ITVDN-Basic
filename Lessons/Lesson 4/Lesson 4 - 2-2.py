df8206a041f2e9962a3793c113f92ce339cb2407
IvanSlivenko <ivan8822@ukr.net>
2024-02-05 20:07:59 +0300

    05-02-2024-20-07

---
+++ Lessons/Lesson 4/Lesson 4 - 2-2.py
@@ -0,0 +1,51 @@
+class Parser: # ------------------------------- Механізми перетворення стічок у число
+    def __init__(self):
+        pass
+    @staticmethod
+    def __converter_type(value_str):
+        result = 0
+        if isinstance(value_str, str):
+            if "." in value_str:
+                result = float(value_str)
+            else:
+                result = int(value_str)
+        return result
+    def parse(self, expression):
+        packed_values = tuple(expression.split(' '))
+        if len(packed_values) < 3:
+            print('Wrong indentation, check your expression')
+            return 0, 0, '+'
+        a, op, b = packed_values
+        return self.__converter_type(a), self.__converter_type(b), op
+
+class Core: #---------------------------------- Механізми обчислення
+    def __init__(self):
+        self.parser = Parser()
+        self._function ={
+            "+": lambda a, b: a + b,
+            "-": lambda a, b: a - b,
+            "/": lambda a, b: a / b,
+            "*": lambda a, b: a * b
+        }
+    def calculate(self,expression ):
+        a, b, op = self.parser.parse(expression)
+        result = self._function.get(op)(a, b)
+        return result
+
+class Interfase: #------------------------------ Застосування механізмів
+    def __init__(self):
+        self.core = Core()
+    def run_calculator(self):
+        while True:
+            print("Enter your expression \n")
+            expression = input()
+            result = self.core.calculate(expression)
+            print('Result : {}'.format(result))
+            print("-" * 10)
+
+if __name__ == "__main__": # ------------------- Запуск кода
+    calculator = Interfase()#---------------------------------------Створюємо екземпляр класу "Interfase"
+    calculator.run_calculator()
+
+
+
--- Lessons/Lesson 4/Lesson 4 - 2.py
+++ Lessons/Lesson 4/Lesson 4 - 2.py
@@ -1,9 +1,8 @@
 class Parser:
     def __init__(self):
         pass
-
     @staticmethod # ---------------------- декоратор статичного методу
-    def __convert_type(value_str):
+    def __convert_type(value_str): #------------------------------------ визначаємо тип очікуваного числа
         result = 0
         if isinstance(value_str, str):
             if "." in value_str:
@@ -12,24 +11,14 @@ class Parser:
                 result = int(value_str)
         return result
 
-    def parse(self, expression):
-        packed_values = tuple(expression.split(''))
-        if len(packed_values)<3:
+    def parse(self, expression):#----------------------------------- Перетворюємо строку у число
+        packed_values = tuple(expression.split(' '))
+        if len(packed_values) < 3:
             print('Wrong indentation, check your expression')
             return 0, 0, '+'
         a, op, b = packed_values
-        return  self.__convert_type(a) ,self.__convert_type(b), op
-
-class Interfase:
-    def __init__(self):
-        self.core = Core()
-
-    def run_calculator(self):
-        print('Enter your expressions')
-        exptessions = input()
-        # result = self.core.
-
-class Core:
+        return self.__convert_type(a), self.__convert_type(b), op
+class Core: #---------------------------------------------------- Виконуємо обчислення
     def __init__(self):
         self.parser = Parser()
         self._function= {
@@ -38,12 +27,22 @@ class Core:
             "/": lambda a, b: a / b,
             "*": lambda a, b: a * b
         }
-
-    def calculate(self,expressoin):
-        a, b, op = self.parser.parse(expressoin)
+    def calculate(self, expression):
+        a, b, op = self.parser.parse(expression)
         result = self._function.get(op)(a, b)
         return result
+class Interfase: #------------------------------------------ Приймаэмо данні та застосовуємо методи
+    def __init__(self):
+        self.core = Core()
 
-if __name__ == "__main__":
+    def run_calculator(self):
+        while True:
+            print('Enter your expressions')
+            expression = input()
+            result = self.core.calculate(expression)
+            print('Result: {}'.format(result))
+            print("=" * 10)
+
+if __name__ == "__main__": #------------------------------ Запускаємо код
     calculator = Interfase()
     calculator.run_calculator()
