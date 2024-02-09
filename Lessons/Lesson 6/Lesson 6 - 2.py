bd345ee0991a6d851de334b76b2a14e445697a62
IvanSlivenko <ivan8822@ukr.net>
2024-02-09 22:12:15 +0300

    09-02-2024-22-12

--- Lessons/Lesson 6/Lesson 6 - 2.py
+++ Lessons/Lesson 6/Lesson 6 - 2.py
@@ -1,30 +1,58 @@
-#------------------------------------ Example 1
+#---------------------------------------------------------------------- Example 1
 chars = []
 for char in 'abcde':
     chars.append(char)
     print(chars)
 print('-'* 30)
 print(chars)
-#---------------------------------- Example 1 - shorts
+
 chars = [char for char in 'abcde']
 print('-'*30)
 print(chars)
 
-#------------------------------------ Example 2
+#------------------------------------------------------------------------ Example 2
 names = ['make','john', 'sally']
 print('-'*30)
 print(names)
-#---------------------------------- Example 2 - shorts
+
 names = [name.capitalize() for name in names]
 print(names)
 print('-'*30)
-
+#------------------------------------------------------------------------ Example 3
 numbers= [1, 2, 3, 4, 5, 6]
 even_nums = [num for num in numbers if num % 2 == 0]
 print(even_nums)
 print('-'*30)
-
+#------------------------------------------------------------------------ Example 4
 unique_values = {value ** 2 for value in [1, 3, 5, 5, 2, 2, 1, 7]}
 print(unique_values)
 print('-'*30)
+#------------------------------------------------------------------------ Example 5
+unique_values_2 = {value for value in [1, 3, 5, 5, 2, 2, 1, 7]}
+print(unique_values_2)
+print('-'*30)
+#------------------------------------------------------------------------ Example 6
+data = ['John_25','Sally_19','Susan_35','Jack_16']
+name_age_dict = {v.split('_')[0]: v.split('_')[1] for v in data}
+print(name_age_dict)
+print('-'*30)
+#------------------------------------------------------------------------ Example 7
+values_squares = {value: value for value in range(10)}
+print(values_squares)
+print('-'*30)
+
+values_squares = {value: value ** 2 for value in range(1, 10)}
+print(values_squares)
+print('-'*30)
+#------------------------------------------------------------------------ Example 8
+m = tuple(n for n in range(1, 12))
+print(m)
+print('-'*30)
+
+#------------------------------------------------------------------------ Example 9
+matrix = [[j for j in range(2)] for i in range(3)]
+print(matrix)
+
+flatten_matrix = [val for sublist in matrix for val in sublist ]
+print(flatten_matrix)
 
