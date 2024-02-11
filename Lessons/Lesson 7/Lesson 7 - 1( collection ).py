f392aeab93ac504f1af115a264f679f60b41c1c7
IvanSlivenko <ivan8822@ukr.net>
2024-02-11 14:29:58 +0300

    11-02-2024-14-29

--- Lessons/Lesson 7/Lesson 7 - 1( collection ).py
+++ Lessons/Lesson 7/Lesson 7 - 1( collection ).py
@@ -1,21 +1,27 @@
 import collections
 
-counts = collections.Counter([1,1,2,3,3,3])
+counts = collections.Counter([1, 1, 2, 3, 3, 3]) # ----------------- Рахуємо кількість входжень кожного числа
 print(counts)
 print('-'*50)
 
-dict_of_list = collections.defaultdict(list)
+a = "Ivan Slivenko"
+counts_2 = collections.Counter(a) # ----------------- Рахуємо кількість входжень кожної букви
+print(counts_2)
+print('-'*50)
 
-for value in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+
+dict_of_list = collections.defaultdict(list) #------------Додаємо невизначені ключі словника
+for value in range(1,10):
     if value % 2 == 0:
         dict_of_list['even'].append(value)
     else:
         dict_of_list['odd'].append(value)
-print(dict_of_list)
+# print(dict_of_list)
+print(dict(dict_of_list))
 print("-"*20)
 
-d = collections.deque([1, 2, 3]) #
-print('d',d)
+d = collections.deque([1, 2, 3]) #------ в перекладі " втостороння черга "
+print('d', d)
 print('-'*20)
 d.popleft() #------------------------------------- Видаляємо зі списку ( зліва )
 print('d popleft', d)
@@ -29,4 +35,5 @@ print('-'*20)
 d.pop() #------------------------------------- Видаляємо зі списку ( справа )
 print('d pop',d)
 print('-'*20)
+
 
