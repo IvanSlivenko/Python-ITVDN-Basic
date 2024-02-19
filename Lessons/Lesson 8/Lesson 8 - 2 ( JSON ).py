8ceb3c0d5e34954212f0a9db93bff1b45b72e6e8
IvanSlivenko <ivan8822@ukr.net>
2024-02-19 16:58:26 +0300

    19-02-2024-37-27

--- Lessons/Lesson 8/Lesson 8 - 2 ( JSON ).py
+++ Lessons/Lesson 8/Lesson 8 - 2 ( JSON ).py
@@ -1,8 +1,21 @@
 import json
 
-file_name = '../../Files/my_file.json'
+file_name = '../../Files/my_file.json' #------------------------------ Знаходимо файл
 
-with open(file_name, 'r') as file:
-    data = json.load(file)
-
+with open(file_name, 'r') as file: #----------------------------- Менеджер контексту для читання
+    data = json.load(file)#----------------------------------------- Записуємо у змінну, прочитаний файл
+    data.append({"name": "Volodumur", "lastName": "Slivenko"})#----- Додаємо у кінець змінної -- data --
+    data.append({"name": "Mariya", "lastName": "Slivenko"})#--
+    data.append({"name": "Anna", "lastName": "Slivenko"})# --
 print(data)
+
+with open('../../Files/my_file_2.json', 'w') as file: #------------- Менеджер контексту для записування
+    data_2 = json.dump(data, file) #-----------Записуємо -- data -- у file
+
+
+with open('../../Files/my_file_2.json', 'r') as file: #------------- Менеджер контексту для читання
+    data_2_1 = json.load(file)# ------------------------------------Записуємо у змінну, прочитаний файл
+
+print('data_2_1', data_2_1)
+
+
